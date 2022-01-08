3ff21dd2218b648ad2ab7931e2925efc5ffd2bac
Stefan Werfling <stefan.werfling@pegenau.de>
2022-01-08 14:22:03 +0100

    update gitignore, update api handler user

--- build/app/Main/User.ts
+++ build/app/Main/User.ts
@@ -1,10 +1,23 @@
 import {Get, JsonController, Session} from 'routing-controllers';
+import {User as UserDB} from '../../inc/Db/MariaDb/Entity/User';
+import {MariaDbHelper} from '../../inc/Db/MariaDb/MariaDbHelper';
+
+/**
+ * UserData
+ */
+export type UserData = {
+    id: number;
+    username: string;
+    email: string;
+    isAdmin: boolean;
+};
 
 /**
  * UserInfo
  */
 export type UserInfo = {
     islogin: boolean;
+    user?: UserData;
 };
 
 /**
@@ -23,12 +36,42 @@ export type UserInfoResponse = {
 export class User {
 
     /**
-     * islogin
+     * getUserInfo
      * @param session
      */
     @Get('/json/user/info')
     public async getUserInfo(@Session() session: any): Promise<UserInfoResponse> {
+        if ((session.user !== undefined) && session.user.isLogin) {
+            const userRepository = MariaDbHelper.getConnection().getRepository(UserDB);
 
+            const user = await userRepository.findOne({
+                where: {
+                    id: session.user.userid
+                }
+            });
+
+            if (user) {
+                return {
+                    status: 'ok',
+                    data: {
+                        islogin: true,
+                        user: {
+                            id: user.id,
+                            username: user.username,
+                            email: user.email,
+                            isAdmin: user.isAdmin
+                        }
+                    }
+                };
+            }
+        }
+
+        return {
+            status: 'ok',
+            data: {
+                islogin: false
+            }
+        };
     }
 
 }
