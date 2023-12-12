3b0bb0b92bcfeb6b686d67e38baa462ad932e3b7
Stefan Werfling <stefan.werfling@pegenau.de>
2023-12-12 10:45:33 +0100

    new version 1.0.31

--- backend/src/inc/Const.ts
+++ backend/src/inc/Const.ts
@@ -3,7 +3,7 @@
  */
 export class Const {
 
-    public static readonly VERSION = '1.0.1';
+    public static readonly VERSION = '1.0.31';
     public static readonly VERSION_API_MOBILE_LOGIN = '1.0.1';
     public static readonly VERSION_API_MOBILE_SYNC = '1.0.2';
     public static readonly VERSION_API_BACKEND = '1.0.1';
